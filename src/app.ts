379388758ab7555c0e93f9d48eefb04c134c53ec
Ravanadevs <123143684+RavanaDevs@users.noreply.github.com>
2023-08-01 10:56:17 +0530

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
 import "dotenv/config";
+import cors from "cors";
 
 import home from "./routes/home";
 import badRequest from "./middlewares/bad-request";
@@ -9,6 +10,7 @@ import productRouter from "./routes/product";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/", home);
 app.use("/products", productRouter);
